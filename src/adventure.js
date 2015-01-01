e349979baad8ca96b1d9b16893f2e3145cb822c0
Terry <tchan247@gmail.com>
2014-12-31 22:12:01 -0800

    Update adventure.js
    - created program loop
    - created key event listener
    - created key lookup table

--- src/adventure.js
+++ src/adventure.js
@@ -1,3 +1,72 @@
 quest.adventure = function() {
+	var key;
 
+	// program interval
+	if(typeof programLoop !== "undefined") clearInterval(programLoop);
+	programLoop = setInterval(runAdventure, 25);
+
+	// program loop
+	function runAdventure() {
+
+		if(key !== undefined) {
+			quest.keyTable[key]();
+			key = undefined;
+		}
+		
+
+	}
+
+	window.addEventListener('keydown',function(e) {
+		e = e || window.e;
+		var keyNum = e.which;
+		if(quest.keyTable) {
+			key = keyNum;
+		}
+
+	})
+
+}
+
+// lookup table for keyboard events
+quest.keyTable = {
+	// enter
+	13 : function() {
+
+	},
+	// space
+	32 : function() {
+		console.log("space");
+	},
+	// arrow left
+	37 : function() {
+
+	},
+	// arrow up
+	38 : function() {
+
+	},
+	// arrow right
+	39 : function() {
+
+	},
+	// arrow down
+	40 : function() {
+
+	},
+	// a
+	65 : function() {
+
+	},
+	// d
+	68 : function() {
+
+	},
+	// s
+	83 : function() {
+
+	},
+	// w
+	87: function() {
+
+	}
 }
