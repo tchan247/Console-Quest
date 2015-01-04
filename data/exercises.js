b8f225e696f42611d70acde13a91cb5550311872
Terry <tchan247@gmail.com>
2015-01-03 23:30:34 -0800

    Create exercises.js
    - add exercises

---
+++ data/exercises.js
@@ -0,0 +1,17 @@
+quest.exercises = {
+	easy : {
+		0 : { 
+			text : "Create a function named addTwo that adds two numbers:",
+			generateCases : false,
+			// sets of arguments
+			cases : {0 : [1,4], 1 : [3,5], [-3, 6], [-5, -3], [50, -99]},
+			test : quest.test.asserts(/* user input */, function()/* .apply(this, cases[0]) */)
+			}
+	},
+	medium : {
+
+	},
+	hard : {
+
+	}
+}
