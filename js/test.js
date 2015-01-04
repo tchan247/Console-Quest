b287275926a3c608ab494963d3da4e7f6444f0b7
Terry <tchan247@gmail.com>
2015-01-03 23:31:53 -0800

    Create test.js
    - added testing object

---
+++ js/test.js
@@ -0,0 +1,15 @@
+quest.test = {
+	asserts : function(a, b) {
+		if(Array.isArray(a)) {
+			for(var i=0, l=a.length; i<l; i++) {
+				if(typeof a[i] === 'number') {
+					
+				}
+			}
+			return true;
+		}
+	},
+	approx : function(a, b, tol) {
+
+	}
+}
