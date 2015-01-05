404dd8b501b2cf03a3cc2a3e25614abcded3ef63
Terry <tchan247@gmail.com>
2015-01-04 23:57:57 -0800

    Update test.js
    - added more to testing functions

--- js/test.js
+++ js/test.js
@@ -1,15 +1,37 @@
 quest.test = {
 	asserts : function(a, b) {
-		if(Array.isArray(a)) {
-			for(var i=0, l=a.length; i<l; i++) {
-				if(typeof a[i] === 'number') {
-					
+		if(typeof a === typeof b) {
+
+			if (Array.isArray(a) && Array.isArray(b)) {
+				for(var i=0, l=a.length; i<l; i++) {
+					if(a[i] !== b[i]) {
+						return false;
+					}
 				}
+				return true;
+			} else if (!Array.isArray(a) && !Array.isArray(b)) {
+				for(var key in a) {
+					if(a[key] !== b[key])
+						return false;
+				}
+			} else {
+				return a === b;
 			}
-			return true;
 		}
+		return false;
 	},
 	approx : function(a, b, tol) {
+		
+		if(typeof a === "number" && typeof b === "number") {
+			if(tol === undefined || typeof tol !== "number") {
+				tol = 0;
+			} else {
+				if(tol < 0) tol *= -1;
+			}
 
+			return Math.abs(a - b) < tol;
+		}
+
+		return false
 	}
 }
